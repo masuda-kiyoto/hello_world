2e8c6c102b494c86ebbb04a7e29c018c6c49f554
masuda-kiyoto <41143374+masuda-kiyoto@users.noreply.github.com>
2019-08-06 11:32:58 +0900

    Update settings.py

--- testapp/settings.py
+++ testapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=p!iz2&djy0c$e%i$p&e_=$&_fs-mjz6p)s1)4vp%r@5ih=&h$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kasuga.tsolpd.com']
+ALLOWED_HOSTS = ['okada.tsolpd.com']
 
 
 # Application definition
