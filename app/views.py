44807655e6e8a717b0349ca2999bf2df00c85640
masuda-kiyoto <41143374+masuda-kiyoto@users.noreply.github.com>
2019-08-07 16:51:47 +0900

    Update views.py

--- app/views.py
+++ app/views.py
@@ -7,6 +7,10 @@ enable('0b6874ae-6834-4e5b-a4de-b314b1297153')
 
 logger = logging.getLogger(__name__)
 
+fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
+
+logging.basicConfig(level=logging.DEBUG, format=fmt)
+
 # Create your views here.
 class Test(View):
     def get(self, request):
