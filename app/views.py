347f0a46b12cc8beeb492ae53d8e6505d18938ad
masuda-kiyoto <41143374+masuda-kiyoto@users.noreply.github.com>
2019-08-07 13:27:56 +0900

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from django.views import View
 from django.shortcuts import render, redirect
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 # Create your views here.
 class Test(View):
     def get(self, request):
+        logger.debug("デバッグ")
+        logger.info("インフォ")
+        logger.warning("ワーニング")
+        logger.error("エラー")
+        logger.critical("クリティカル")
         return render(request, 'hello_world.html')
