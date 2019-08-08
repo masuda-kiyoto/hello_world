43c8ea5923a55470d5e09f062c8e68d65f93254c
masuda-kiyoto <masuda_kiyoto@tsolweb.co.jp>
2019-08-08 15:10:31 +0900

    log

--- app/views.py
+++ app/views.py
@@ -1,18 +1,16 @@
 from django.views import View
 from django.shortcuts import render, redirect
 import logging
-from applicationinsights.logging import enable
-
-enable('0b6874ae-6834-4e5b-a4de-b314b1297153')
 
 logger = logging.getLogger(__name__)
 
-fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
-
-logging.basicConfig(level=logging.DEBUG, format=fmt)
-
 # Create your views here.
 class Test(View):
+
+    fmt = "%(asctime)s %(levelname)s %(name)s :%(message)s"
+
+    logging.basicConfig(level=logging.DEBUG, format=fmt)
+
     def get(self, request):
-        logger.warning("ワーニング")
+        logger.warning("Error Test")
         return render(request, 'hello_world.html')
