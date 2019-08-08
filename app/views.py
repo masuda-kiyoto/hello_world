29fe4e7c8dcade24ec8f2d76b2b1e4e4e037e7f9
masuda-kiyoto <masuda_kiyoto@tsolweb.co.jp>
2019-08-08 14:05:00 +0900

    Application Insights log

--- app/views.py
+++ app/views.py
@@ -1,18 +1,12 @@
 from django.views import View
 from django.shortcuts import render, redirect
 import logging
-from applicationinsights.logging import enable
-
-enable('095f40cf-8be3-416f-8d93-c12b17d178b2')
-
-logger = logging.getLogger(__name__)
-
-fmt = "%(message)s"
-
-logging.basicConfig(level=logging.DEBUG, format=fmt)
 
 # Create your views here.
 class Test(View):
+
+    logger = logging.getLogger(__name__)
+
     def get(self, request):
         logger.error(" Error Test ")
         return render(request, 'hello_world.html')
