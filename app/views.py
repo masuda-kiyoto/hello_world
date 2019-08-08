f944be7f88862098e44505adbce82a25fda27178
masuda-kiyoto <masuda_kiyoto@tsolweb.co.jp>
2019-08-08 16:02:02 +0900

    log

--- app/views.py
+++ app/views.py
@@ -12,9 +12,9 @@ class Test(View):
     logging.basicConfig(level=logging.DEBUG, format=fmt)
 
     def get(self, request):
-        logger.debug('This is debug message.')
-        logger.info('This is info message.')
-        logger.warn('This is warning message.')
-        logger.error('This is error message.')
-        logger.critical('This is critical message.')
+
+        test = None
+
+        error = 3 + test
+
         return render(request, 'hello_world.html')
