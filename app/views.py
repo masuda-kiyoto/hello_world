df78d283d9b1db0c32b8b210262a9a0ce0453282
masuda-kiyoto <masuda_kiyoto@tsolweb.co.jp>
2019-08-08 14:07:09 +0900

    Application Insights log

--- app/views.py
+++ app/views.py
@@ -5,8 +5,7 @@ import logging
 # Create your views here.
 class Test(View):
 
-    logger = logging.getLogger(__name__)
-
     def get(self, request):
+        logger = logging.getLogger(__name__)
         logger.error(" Error Test ")
         return render(request, 'hello_world.html')
