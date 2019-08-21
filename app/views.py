040680e6be7f6e68545c1a936bea44b915e8cfea
masuda-kiyoto <masuda_kiyoto@tsolweb.co.jp>
2019-08-21 14:41:53 +0900

    fix db connect

--- app/views.py
+++ app/views.py
@@ -10,25 +10,26 @@ class Test(View):
 
     def get(self, request):
 
-        connect = mysql.connector.connect(
-            database='test_table',
-            user="masuda@employee-ledger",
-            charset='utf8'
-        )
-
-        cur = connect.cursor(dictionary=True)
-
-        connect.commit()
-        cur.execute('SELECT * FROM test_table ')
-        rows = cur.fetchall()
-        cur.close()
-        connect.close()
+        # connect = mysql.connector.connect(
+        #     database='test_table',
+        #     user="masuda@employee-ledger",
+        #     charset='utf8'
+        # )
+        #
+        # cur = connect.cursor(dictionary=True)
+        #
+        # connect.commit()
+        # cur.execute('SELECT * FROM test_table ')
+        # rows = cur.fetchall()
+        # cur.close()
+        # connect.close()
 
         # logger.debug('This is debug message.')
         # logger.info('This is info message.')
         # logger.warn('This is warning message.')
         # logger.error('エラーだよ！')
         # logger.critimancal('重大なエラーだよ！')
-        return render(request, 'hello_world.html', context={
-            'data': rows
-        })
+        # , context={
+        #             'data': rows
+        #         }
+        return render(request, 'hello_world.html')
